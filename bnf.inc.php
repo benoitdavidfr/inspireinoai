96c8b7cf988be965feb0f805782b824c52888c0b
Benoit DAVID <benoit.davi@free.fr>
2018-02-26 02:45:57 +0000

    add php

---
+++ bnf.inc.php
@@ -0,0 +1,292 @@
+<?php
+/*PhpDoc:
+name: bnf.inc.php
+title: Classes et fonctions utilisées pour décrire une BNF
+classes:
+doc: |
+  voir https://fr.wikipedia.org/wiki/Forme_de_Backus-Naur
+  Une BNF est décrite par un objet de la classe BNF
+  A chaque classe (sauf BNf) est associée une fonction constructeur
+  voir l'exemple de la définition du langage de requêtes sur les métadonnées
+*/
+
+/*PhpDoc: classes
+name: class BNF
+title: classe ensemble de règles BNF
+doc: |
+  Un objet correspond à un ensemble de règles BNF et un symbole de départ
+  A chaque règle un commentaire est associé.
+  L'ensemble de règles est défini par un array de la forme: [ $comment => $rule ]
+  où:
+    - $comment est le commentaire décrivant la règle
+    - $rule est un objet Rule qui définit formellement la règle
+*/
+class BNF {
+  private $start; // le symbole de départ: string
+  private $rules; // les règles: [ symbol => [['comment'=>comment, 'def'=>Def]]]
+  
+  function __construct(string $start /* symbole de départ */, array $rules) { 
+// $rules: les règles: [ comment:string => rule:Rule]
+    $this->start = $start;
+    $this->rules = [];
+    foreach ($rules as $comment => $rule) {
+      $symbol = $rule->symbol();
+      if (!isset($this->rules[$symbol]))
+        $this->rules[$symbol] = [];
+      $this->rules[$symbol][] = [
+        'comment'=> $comment,
+        'def'=> $rule->def(),
+      ];
+      $rule->def()->setBnf($this);
+    }
+  }
+  
+  function __toString() {
+    $str = "<ul>\n";
+    foreach ($this->rules as $symbol => $rules) {
+      foreach ($rules as $rule) {
+        $rule2 = Rule($symbol, $rule['def']);
+        $str .= "<li>".$rule2." // $rule[comment]\n";
+      }
+    }
+    $str .= "</ul>\n";
+    return $str;
+  }
+  
+  // teste si un texte est conforme à la définition d'un symbole, si conforme alors renvoie le texte restant sinon false
+  function checkSymbol(string $text, string $symbol, bool $verbose=true) {
+    foreach ($this->rules[$symbol] as $rule) {
+      //echo "<pre>rule="; print_r($rule); echo "</pre>\n";
+      if ($verbose)
+        echo "test BNF de $rule[comment]<br>\n";
+      $check = $rule['def']->check($text, $verbose);
+      if ($check === false) {
+        if ($verbose)
+          echo "test BNF $symbol ko de $rule[comment]<br>\n";
+      }
+      else {
+        if ($verbose)
+          echo "test BNF $symbol OK de $rule[comment] returns \"$check\"<br>\n";
+        return $check;
+      }
+    }
+    if ($verbose)
+      echo "test BNF ko pour $symbol<br\n";
+    return false;
+  }
+  
+  // teste si un texte est conforme à la BNF, si conforme alors renvoie le texte restant sinon false
+  function check(string $text, bool $verbose=true) {
+    return $this->checkSymbol($text, $this->start, $verbose);
+  }
+};
+
+/*PhpDoc: classes
+name: class Rule
+title: classe Règle BNF définissant un symbole
+doc: |
+  Un objet correspond à une règle BNF définissant un symbole
+  La définition est un objet Def
+*/
+class Rule {
+  private $symbol; // symbole défini: string
+  private $def; // définition: Def
+  function symbol() { return $this->symbol; }
+  function def() { return $this->def; }
+  function __construct(string $symbol, Def $def) { $this->symbol = $symbol; $this->def = $def; }
+  function __toString() { return '{'.$this->symbol.'} ::= '.$this->def; }
+};
+function Rule(string $symbol, Def $def) { return new Rule($symbol, $def); }
+
+/*PhpDoc: classes
+name: class Def
+title: Super-classe des diverses définitions possibles
+*/
+class Def {};
+
+/*PhpDoc: classes
+name: class Seq extends Def
+title: Séquence d'objets plus option
+doc: |
+  Séquence d'objets Term, Symb, Choice, RegExp
+  L'option vaut '' | '?' | '+' | '*'
+*/
+class Seq extends Def {
+  private $seq; // array(Seq|Choice|Term|Symb|RegExp)
+  private $option; // string : '' | '?' | '+' | '*'
+  
+  function __construct(array $seq, string $option) { $this->seq = $seq; $this->option = $option; }
+  
+  function setBnf(BNF $bnf) { foreach ($this->seq as $elt) { $elt->setBnf($bnf); } }
+    
+  function __toString() {
+    return (!$this->option ? implode(' ',$this->seq) : '('.implode(' ',$this->seq).')'.$this->option);
+  }
+  
+  // teste si un texte est conforme à un élément, si conforme alors renvoie le texte restant sinon false
+  function checkOneElt(string $text, $elt, bool $verbose=true) {
+    if ($verbose)
+      echo "Test Seq1 de $elt<br>\n";
+    $check = $elt->check($text, $verbose);
+    if ($check === false) {
+      if ($verbose)
+        echo "test Seq1 ko de $elt<br>\n";
+      return false;
+    }
+    else {
+      if ($verbose)
+        echo "test Seq1 OK de $elt<br>\n";
+      return $check;
+    }
+  }
+  
+  // teste si un texte est conforme, si conforme alors renvoie le texte restant sinon false
+  function check(string $text0, bool $verbose=true) {
+    //echo "<pre>seq="; print_r($this->seq); echo "</pre>\n";
+    $text = $text0;
+    foreach ($this->seq as $elt) {
+      echo "Test Seq de $elt<br>\n";
+      $check = $this->checkOneElt($text, $elt, $verbose); 
+      if ($check === false)
+        break;
+      else
+        $text = $check;
+    }
+    if ($check === false) {
+      switch ($this->option) {
+        case '':
+        case '+':
+          return false;
+        case '*':
+        case '?':
+          return $text0;
+      }
+    }
+    // la première itération est OK
+    if (in_array($this->option, ['','?']))
+      return $text;
+    while(true) {
+      foreach ($this->seq as $elt) {
+        $check = checkOneElt($text, $elt, $verbose); 
+        if ($check === false)
+          break;
+        else
+          $text = $check;
+      }
+      if ($check === false)
+        return $text;
+    }
+  }
+};
+function Seq(array $seq, string $option='') {
+  foreach ($seq as $obj) {
+    if (!in_array(get_class($obj),['Seq','Choice','Term','Symb','RegExp']))
+      throw new Exception("dans Seq: objet de classe ".get_class($obj)." non prévue");
+    //echo "get_class=",get_class($obj),"<br>\n";
+  }
+  if (!in_array($option,['','?','+','*']))
+    throw new Exception("dans Seq: option='$option' non prévue");
+  return new Seq($seq, $option);
+}
+
+/*PhpDoc: classes
+name: class Choice extends Def
+title: Choix entre différentes possibilités
+*/
+class Choice extends Def {
+  private $choices; // array(Seq|Term|Symb|RegExp)
+  function __construct(array $choices) { $this->choices = $choices; }
+  function setBnf(BNF $bnf) { foreach ($this->choices as $elt) { $elt->setBnf($bnf); } }
+  function __toString() { return '('.implode(' | ',$this->choices).')'; }
+  function check(string $text, bool $verbose=true) {
+    foreach ($this->choices as $choice) {
+      $check = $choice->check($text, $verbose);
+      if ($check !== false) {
+        if ($verbose)
+          echo "Test Choice OK pour $choice returns \"$check\"<br>\n";
+        return $check;
+      }
+    }
+    if ($verbose)
+      echo "Test Choice ko pour $this<br>\n";
+    return false;
+  }
+};
+function Choice(array $choices) {
+  foreach ($choices as $obj) {
+    if (!in_array(get_class($obj),['Seq','Term','Symb','RegExp']))
+      throw new Exception("dans Choice: objet de classe ".get_class($obj)." non prévue");
+    //echo "get_class=",get_class($obj),"<br>\n";
+  }
+  return new Choice($choices);
+}
+
+/*PhpDoc: classes
+name: class Term
+title: Terminal correspondant à une chaine
+*/
+class Term {
+  private $str; // string
+  function __construct(string $str) { $this->str = $str; }
+  function setBnf(BNF $bnf) { }
+  function __toString() { return "'".$this->str."'"; }
+  
+  // teste si un texte est conforme, si conforme alors renvoie le texte restant sinon false
+  function check(string $text, bool $verbose=true) {
+    if (strncmp($text, $this->str, strlen($this->str))==0) {
+      $ret = substr($text, strlen($this->str));
+      if ($verbose)
+        echo "Test Term OK '$this->str' returns \"$ret\"<br>\n";
+      return $ret;
+    }
+    else {
+      if ($verbose)
+        echo "Test Term ko '$this->str'<br>\n";
+      return false;
+    }
+  }
+};
+function Term(string $str) { return new Term($str); }
+
+/*PhpDoc: classes
+name: class Symb
+title: Non-terminal référençant un symbole dans une définition
+*/
+class Symb {
+  private $str; // string
+  private $bnf; // référence vers la BNF dans laquelle est définie le symbole
+  function __construct(string $str) { $this->str = $str; }
+  function setBnf(BNF $bnf) { $this->bnf = $bnf; }
+  function __toString() { return '{'.$this->str.'}'; }
+  // teste si un texte est conforme, si conforme alors renvoie le texte restant sinon false
+  function check(string $text, bool $verbose=true) {
+    return $this->bnf->checkSymbol($text, $this->str, $verbose);
+  }
+};
+function Symb($str) { return new Symb($str); }
+
+/*PhpDoc: classes
+name: class RegExp extends Def
+title: Terminal correspondant à une expression régulière
+*/
+class RegExp extends Def {
+  private $pattern; // string, ne comprend ni limiteur ni ^ ni $
+  function __construct(string $pattern) { $this->pattern = $pattern; }
+  function setBNF(BNF $bnf) {}
+  function __toString() { return 'RegExp('.$this->pattern.')'; }
+  
+  // teste si un texte est conforme, si conforme alors renvoie le texte restant sinon false
+  function check(string $text, bool $verbose=true) {
+    echo "pattern=$this->pattern<br>\n";
+    echo "text=$text<br>\n";
+    if (!preg_match('!^([^/]*)!', $text, $matches))
+      throw new Exception("No match");
+    $subtext = $matches[1];
+    $pattern = $this->pattern;
+    if (preg_match("!^$pattern!", $subtext, $matches))
+      return substr($text, strlen($matches[0]));
+    else
+      return false;
+  }
+};
+function RegExp($str) { return new RegExp($str); }
---
+++ mdquery.php
@@ -0,0 +1,208 @@
+<?php
+/*PhpDoc:
+name: mdquery.php
+title: analyse d'une requête sur les métadonnées
+doc: |
+    {expr} ::= or({expr}, {expr}, ...)
+    {expr} ::= and({expr}, {expr}, ...) 
+    {expr} ::= not({expr})
+    {expr} ::= regExp({regExp}, {eltStr}, {options}) // regexp sur {eltStr} avec {options}
+    {eltStr} ::= title | abstract | locator | uri | operatesOn | lineage | useLimitation // les elts de type text ou array(text)
+    {eltStr} ::= responsibleParty.name | responsibleParty.email | mdContact.name | mdContact.email
+    {expr} ::= equals({eltEnum}, {string})
+    {eltEnum} ::= type | serviceType | accessConstraints | language
+    {expr} ::= contains({eltArrayEnum}, {string})
+    {eltArrayEnum} ::= resourceLanguage | topicCategory
+    {expr} ::= hasKeyword({string}, {string}) // le premier paramètre est soit le libellé soit un uri,
+                                           // le second optionnel est l'URI ou un regexp sur le titre du CVOC
+    {expr} ::= intersects({nombre},{nombre},{nombre},{nombre}) // intersection avec la bbox
+    {expr} ::= between({date},{date}) | before({date}) | after({date})
+    {expr} ::= lessThan({resolution}, {number}) | greaterThan({resolution}, {number})
+    {resolution} ::= 'max(spatialResolutionDistance)' | 'min(spatialResolutionDistance)'
+    {resolution} ::= 'max(spatialResolutionScaleDenominator)' | 'min(spatialResolutionScaleDenominator)'
+    {expr} ::= conformsTo({spec}) | doesntConformTo({spec})
+    {spec} ::= {uri} | {regexpOfTitle}
+    {expr} ::= mdBetween({date},{date}) | mdBefore({date}) | mdAfter({date})
+*/
+require_once __DIR__.'/bnf.inc.php';
+
+// Spécifications du langage de requêtes des métadonnées
+$specs = [
+  "conjonction d'expressions"
+    => Rule('expr', Seq([Term('and('), Symb('expr'), Seq([Term(','),Symb('expr')],'*'), Term(')')])),
+  "disjonction d'expressions"
+    => Rule('expr', Seq([Term('or('), Symb('expr'), Seq([Term(','),Symb('expr')],'*'), Term(')')])),
+  "négation d'une expression"
+    => Rule('expr', Seq([Term('not('), Symb('expr'), Term(')')])),
+  "exp. régulière sur un élément de type chaine ou array(chaine) avec une option éventuelle"
+    => Rule('expr', Seq([Term('regExp('), Symb('regExp'), Term(','), Symb('eltStr'), 
+                    Seq([Term(','), Symb('string')],'?'), Term(')')])),
+  "les éléments de type chaine ou array(chaine)"
+    => Rule('eltStr', Choice([
+            Term('title'),Term('abstract'),Term('locator'),Term('uri'),Term('operatesOn'),
+            Term('lineage'),Term('useLimitation'),
+            Term('responsibleParty.name'),Term('responsibleParty.email'),
+            Term('mdContact.name'),Term('mdContact.email')
+          ])),
+  "test d'égalité entre un élément de type Enum et une valeur"
+    => Rule('expr', Seq([Term('equals('), Symb('eltEnum'), Term(','), Symb('string'), Term(')')])),
+  "les éléments de type Enum"
+    => Rule('eltEnum', Choice([Term('type'),Term('serviceType'),Term('accessConstraints'),Term('language')])),
+  "test d'appartenance pour un élément de type array(Enum) et une valeur"
+    => Rule('expr', Seq([Term('contains('), Symb('eltArrayEnum'), Term(','), Symb('string'), Term(')')])),
+  "les éléments de type array(Enum)"
+    => Rule('eltArrayEnum', Choice([Term('resourceLanguage'),Term('topicCategory')])),
+  "existence d'un mot-clé défini par soit son libellé soit son URI plus éventuellement l'URI ou un RegExp"
+      ." du titre du CVOC"
+    => Rule('expr', Seq([Term('hasKeyword('), Symb('string'), 
+                        Seq([Term(','), Choice([Symb('string'),Symb('regExp')])],'?'),
+                        Term(')')])),
+  "intersection avec un rectangle englobant défini par westBoundLongitude, southBoundLatitude, "
+      ."eastBoundLongitude, northBoundLatitude"
+    => Rule('expr', Seq([Term('intersects('), Symb('number'), Term(','), Symb('number'), Term(','),
+                                             Symb('number'), Term(','), Symb('number'), Term(')')])),
+  "date sur les données avant la date"
+    => Rule('expr', Seq([Term('before('), Symb('date'), Term(')')])),
+  "date sur les données après la date"
+    => Rule('expr', Seq([Term('after('), Symb('date'), Term(')')])),
+  "resolution plus petite qu'une valeur"
+    => Rule('expr', Seq([Term('lessThan('), Symb('resolution'), Term(','), Symb('number'), Term(')')])),
+  "resolution plus grande qu'une valeur"
+    => Rule('expr', Seq([Term('greaterThan('), Symb('resolution'), Term(','), Symb('number'), Term(')')])),
+  "resolution"
+    => Rule('resolution', Choice([
+                            Term('max(spatialResolutionDistance)'),
+                            Term('min(spatialResolutionDistance)'),
+                            Term('max(spatialResolutionScaleDenominator)'),
+                            Term('min(spatialResolutionScaleDenominator)')])),
+  "conformité à une spec"
+    => Rule('expr', Seq([Term('conformsTo('), Symb('spec'), Term(')')])),
+  "non conformité à une spec"
+    => Rule('expr', Seq([Term('doesntConformTo('), Symb('spec'), Term(')')])),
+  "la spec est définie par un URI ou par un RegExp sur son titre"
+    => Rule('spec', Choice([Symb('string'),Symb('regExp')])),
+  "date de mise à jour des métadonnées avant la date"
+    => Rule('expr', Seq([Term('mdBefore('), Symb('date'), Term(')')])),
+  "date de mise à jour des métadonnées après la date"
+    => Rule('expr', Seq([Term('mdAfter('), Symb('date'), Term(')')])),
+  "définition d'une chaine par une expression régulière"
+    => Rule('string', RegExp("'[^']*'")),
+  "définition d'un nombre par une expression régulière"
+    => Rule('number', RegExp('[0-9]+(\.[0-9]+)?')),
+  "définition d'une expression régulière entre 2 /"
+    => Rule('regExp', RegExp('/[^/]*/')),
+  "définition d'une date par une expression régulière"
+    => Rule('date', RegExp('[0-9][0-9][0-9][0-9](-[0-9][0-9](-[0-9][0-9])?)?')),
+];
+$specs = new BNF('expr', $specs);
+
+echo "<html><head><meta charset='UTF-8'><title>mdquery</title></head><body>\n";
+switch (isset($_GET['action']) ? $_GET['action'] : null) {
+  case 'spec':
+    echo  "<h2>Query specifications</h2>\n",
+          $specs,
+          "<a href='?action=examples'>query examples</a>\n";
+    echo "<pre>"; print_r($specs);
+    die();
+    
+  case 'examples':
+    echo  "<h2>Query examples</h2>\n<ul>\n";
+    foreach([
+      "titre contient eau" => "regExp(/eau/,title,'i')",
+      "type = 'dataset'" => "equals(type,'dataset')",
+      "topicCategory contains 'boundaries'" => "contains(topicCategory,'boundaries')",
+      "contient le mot-clé 'urbanisme'" => "hasKeyword('urbanisme')",
+      "contient le mot-clé 'http://inspire.ec.europa.eu/theme/lu'"
+         => "hasKeyword('http://inspire.ec.europa.eu/theme/lu')",
+      "contient le mot-clé 'urbanisme' du CVOC URBAMET" => "hasKeyword('urbanisme',/URBAMET/)",
+      "responsibleParty.name contient DREAL" => "regExp(/DREAL/, responsibleParty.name, 'i')",
+      "intersects a bbox" => "intersects(0.0, 45.0, 1.0, 46.0)",
+      "date entre 2 dates" => "between(2010,2015)",
+      "date postérieure à une date" => "after(2010)",
+      "resolution" => "lessThan(max(spatialResolutionScaleDenominator), 1000)",
+      "conforme à la spec usage du sol" => "conformsTo('http://inspire.ec.europa.eu/theme/lu')",
+      "conforme à la spec CNIG des PLU" => "conformsTo(/CNIG-PLU/)",
+      "date des MD postérieure à une date" => "mdAfter(2018-01-15)",
+      "titre contient eau et date des MD postérieure à une date"
+         => "and(regExp(/eau/, title, 'i'),mdAfter(2018-01-15))",
+    ] as $title => $query)
+      echo "<li><a href='?query=",urlencode($query),"'>$title\n";
+    echo "</ul>\n";
+    die();
+}
+
+$query = (isset($_GET['query']) ? htmlspecialchars($_GET['query']) : '' );
+echo <<<EOT
+  <h2>Query</h2>
+  <table border=1><form>
+    <tr><td>
+      <textarea rows=20 cols=90 name='query'>$query</textarea>
+    </td></tr>
+    <tr><td>
+    <center><input type='submit' value='Envoi'></center>
+    </td></tr>
+  </form></table>
+  <a href='?action=spec'>query specifications</a>,
+  <a href='?action=examples'>query examples</a><br>
+EOT;
+if (!isset($_GET['query']))
+  die("empty query<br>\n</body></html>\n");
+
+class Tree {
+  private $label; // string
+  private $children; // array des enfants
+  
+  // retourne la chaine restant à analyser
+  function analyze(string $token, string $query): string {
+    if ($token == 'expr') {
+      if (!preg_match('!^([^(]+)\(!', $query, $matches))
+        throw new Exception("No match line ".__LINE__);
+      //echo "<pre>matches="; print_r($matches); echo "</pre>\n";
+      $this->label = $matches[1];
+      $query = substr($query, strlen($matches[0]));
+      if (in_array($matches[1],['and','or','not']))
+        $token = 'expr';
+      else
+        $token = 'param';
+      $this->children[0] = new Tree;
+      $query = $this->children[0]->analyze($token, $query);
+      //echo "res=$query";
+      while (substr($query,0,1)==',') {
+        $query = substr($query,1);
+        $child = new Tree;
+        $this->children[] = $child;
+        $query = $child->analyze($token, $query);
+        //echo "res=$query";
+      }
+      if (substr($query,0,1)==')')
+        return substr($query,1);
+      else
+        throw new Exception("No match line ".__LINE__);
+    }
+    elseif ($token == 'param') {
+      
+      if (preg_match('!^(/[^/]+/)!', $query, $matches) // regexp
+          or preg_match('!^ *([a-z.]+)!i', $query, $matches) // nom d'élément
+          or preg_match("!^ *('[^']*')!", $query, $matches) // chaine entre '
+          or preg_match("!^ *([0-9][-0-9.]*)!", $query, $matches)) { // nombre ou date
+        //echo "<pre>matches="; print_r($matches); echo "</pre>\n";
+        $this->label = $matches[1];
+        return substr($query,strlen($matches[0]));
+      }
+      else
+        throw new Exception("No match line ".__LINE__);
+    }
+    else
+      throw new Exception("No match line ".__LINE__);
+  }
+}
+/*
+$tree = new Tree;
+$tree->analyze('expr', $_GET['query']);
+echo "<pre>tree="; print_r($tree); echo "</pre>\n";
+*/
+$result = $specs->check($_GET['query']);
+if ($result == '')
+  echo "résultat vide<br\n";
+else
+  echo "result = \"$result\"\n";
